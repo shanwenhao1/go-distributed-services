ca81b83da78a81111b562fc8b1d7b5864792befe
swh <761542858@qq.com>
2019-04-17 18:55:26 +0800

    cluster remain error

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	"github.com/AsynkronIT/goconsole"
-	"github.com/AsynkronIT/protoactor-go/actor"
-	"go-distributed-services/domain/actor_example"
-)
-
 func main() {
-	props := actor.PropsFromProducer(func() actor.Actor {
-		return &actor_example.HelloActor{}
-	})
-	rootContext := actor.EmptyRootContext
-	pid := rootContext.Spawn(props)
-	rootContext.Send(pid, &actor_example.Hello{Who: "Roger"})
-	console.ReadLine()
 }
